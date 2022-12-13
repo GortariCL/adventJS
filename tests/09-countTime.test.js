c5080927b956a401facaca98feb3abab7a7de0a4
Fernando Gortari <fernando.gortari@gmail.com>
2022-12-13 11:01:24 -0300

    adventJS checkPart and countTime challenges are done

--- tests/09-countTime.test.js
+++ tests/09-countTime.test.js
@@ -1,50 +1,28 @@
-/*
-Una empresa que fabrica luces de Navidad nos ha pedido que le echemos una mano.
-
-Tienen unas tiras led que son como un Array. Cada posición es un led y puede 
-ser estar encendido (1) o apagado (0).
-
-Cada 7 segundos, los leds cambian de estado de esta forma:
-
-Si el led está apagado, se enciende si el led de su izquierda (index - 1) estaba 
-encendido antes.
-Si el led está encendido, se mantiene siempre encendido.
-Nos han pedido un programa que nos diga cuantos segundos deben pasar hasta que todos 
-los leds están encendidos. Los segundos se expresan en un número entero. Por ejemplo:
-
-const leds = [0, 1, 1, 0, 1]
-countTime(leds) // 7
-// 7 segundos ya que en el primer cambio
-// todas las luces se encendieron
-// 0s: [0, 1, 1, 0, 1]
-// 7s: [1, 1, 1, 1, 1]
-
-countTime([0, 0, 0, 1]) // 21
-// 21 segundos ya que necesita tres cambios:
-// 0s: [0, 0, 0, 1]
-// 7s: [1, 0, 0, 1]
-// 14s: [1, 1, 0, 1]
-// 21s: [1, 1, 1, 1]
-
-countTime([0, 0, 1, 0, 0]) // 28
-// 28 segundos ya que necesita cuatro cambios:
-// 0s: [0, 0, 1, 0, 0]
-// 7s: [0, 0, 1, 1, 0]
-// 14s: [0, 0, 1, 1, 1]
-// 21s: [1, 0, 1, 1, 1]
-// 28s: [1, 1, 1, 1, 1]
-A tener en cuenta
-El array siempre tendrá al menos un led encendido.
-El array puede tener cualquier longitud.
-Si todos los leds están encendidos, el tiempo es 0.
-*/
-
 import { describe, expect, it } from "vitest";
-
-const countTime = () => { };
+import { countTime } from "../src/09-countTime";
 
 describe("given the cuntTime function", () => {
     it("should be a function", () => {
         expect(typeof countTime).toBe("function");
+    });
+
+    it("should throw an error if not provider parameter", () => {
+        expect(() => countTime()).toThrow();
+    });
+
+    it("should throw an error if parameter is an empty array", () => {
+        expect(() => countTime([])).toThrow();
+    });
+
+    it("should throw an error if the data in the array is not a number", () => {
+        expect(() => countTime(["countTime", true])).toThrow();
+    });
+
+    it("should throw an error if the data in the array is another like 1 or 0", () => {
+        expect(() => countTime([1, 2])).toThrow();
+    });
+
+    it("given an array [0, 0, 0, 1] should return 21", () => {
+        expect(countTime([0, 0, 0, 1])).toEqual(21);
     });
 });
